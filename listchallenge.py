9ed357d15da30da49472a576bd6960bab8e7c917
dwegner90 <dawegner90@gmail.com>
2023-05-24 15:47:24 +0000

    Learning about dictionaries

---
+++ listchallenge.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+heroes= ["Spiderman", "Batman", "Black Panther", "Wonder Woman", "Storm"]
+
+# PART 1
+# Print out your favorite character from this list! The output would look something like:
+print(f"My favorite character is {heroes[2]}!")
+
+
+# PART 2
+# Ask the user to pick a number between 1 and 100.
+# Convert the input into an integer.
+
+pick= int(input("Pick a number between 1 and 100\n"))
+
+
+
+
+nums= [1, -5, 56, 987, 0]
+
+# PART 3
+# check out https://docs.python.org/3/library/functions.html or go to Google
+# use a built-in function to find which integer in nums is the biggest.
+# then, print out that number!
+print(max(nums))
+#!/usr/bin/python
+
+#heroes= ["Spiderman", "Batman", "Black Panther", "Wonder Woman", "Storm"]
+
+# PART 1
+# Print out your favorite character from this list! The output would look something like:
+# My favorite character is Black Panther!
+
+
+# PART 2
+# Ask the user to pick a number between 1 and 100.
+# Convert the input into an integer.
+
+
+nums= [1, -5, 56, 987, 0]
+
+# PART 3
+# check out https://docs.python.org/3/library/functions.html or go to Google
+# use a built-in function to find which integer in nums is the biggest.
+# then, print out that number!
+
---
+++ listmethods/listmeth01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/e"https"]
+print(proto)nv python3
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+print(proto)
+print(proto[1])
+proto.extend("dns") # this line will add d, n, and s
+print(proto)
+proto = ["ssh", "http", "https"]
+print(proto)
+print(proto[1])
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+print(proto)
+
